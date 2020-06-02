d0265a530fcb8ca59895aac6b9d5d8b6a4b52f3f
swaroop-nath <swaroopnath6@gmail.com>
2020-06-02 11:58:58 +0530

    Debugging in progress, 2 problems identified -> identified_problems.txt

--- Summarizing Module/chunk_graphs.py
+++ Summarizing Module/chunk_graphs.py
@@ -3,6 +3,11 @@ from text_utils import get_chunks, simple_pre_process, rigorous_pre_process, tf_
 from utils import find_distance
 import manager as mgr
 from nltk.tokenize import word_tokenize
+import logging
+from copy import copy
+
+START_TOKEN = '<<START>>'
+END_TOKEN = '<<END>>'
 
 class UndefinedChunkError(Exception):
     def __init__(self, message):
@@ -15,7 +20,9 @@ def extract_possible_path(cluster: List[str], abbrevs: List[str]) -> str:
     are_all_sentences_same = _check_for_similarity(cluster)
     if are_all_sentences_same: return cluster[0]
 
+    logging.info('method: extract_possible_path- Constructing Chunk Graph')
     chunk_graph, chunk_mapper, word_tf_idf = _construct_chunk_graph(cluster, abbrevs)
+    logging.info('method: extract_possible_path- Getting the possible paths in the Chunk Graph')
     possible_paths = _get_possible_paths(chunk_graph, chunk_mapper, word_tf_idf)
     
     return possible_paths
@@ -28,7 +35,9 @@ def _check_for_similarity(cluster: List[str]) -> str:
     return True
 
 def _construct_chunk_graph(cluster: List[str], abbrevs: Dict[str, str]) -> (Dict[str, List[str]], Dict[str, List[str]], Dict[str, float]):
+    logging.info('method: _construct_chunk_graph- Extracting chunks for every sentence in the cluster')
     sentence_wise_chunks = _chunk_sentences(cluster)
+    logging.info('method: _construct_chunk_graph- Extracting tf-idf values for heuristic scoring')
     chunk_mapper, word_tf_idf = _get_tf_idf_values(sentence_wise_chunks, abbrevs)
     global START_TOKEN
     global END_TOKEN
@@ -36,6 +45,7 @@ def _construct_chunk_graph(cluster: List[str], abbrevs: Dict[str, str]) -> (Dict
     END_TOKEN = '<<END>>'
     chunk_graph = {START_TOKEN: []}
 
+    logging.info('method: _construct_chunk_graph- Chunk Graph Construction Start . . . . ')
     for chunked_sentence in sentence_wise_chunks:
         chunk_graph[START_TOKEN].append(chunked_sentence[0])
         for chunk_idx in range(len(chunked_sentence) - 1):
@@ -49,6 +59,7 @@ def _construct_chunk_graph(cluster: List[str], abbrevs: Dict[str, str]) -> (Dict
     
     for start_node, children_nodes in chunk_graph.items():
         chunk_graph[start_node] = list(set(children_nodes))
+    logging.info('method: _construct_chunk_graph- Chunk Graph Construction done')
 
     return chunk_graph, chunk_mapper, word_tf_idf
 
@@ -90,10 +101,12 @@ def _get_possible_paths(chunk_graph: Dict[str, List[str]], chunk_mapper: Dict[st
     beam = [(chunk, str(path_number)) for path_number, chunk in enumerate(chunk_graph[START_TOKEN])]
     reverse_chunk_mapper = _reverse_chunk_map(chunk_mapper)
 
+    logging.info('method: _get_possible_paths- Starting Beam Search . . . . ') 
     while len(beam) > 0:
-        FLAG_END_TOKEN_CHILD = False
+        END_TOKEN_CHILD = -1
         chunk, path_id = beam.pop(0)
         if possible_paths.get(path_id) is None: possible_paths[path_id] = [chunk]
+        else: possible_paths[path_id].append(chunk)
 
         # Checking for children nodes, and judging value of adding them to the path based on a heuristic value
         children_chunks = chunk_graph[chunk]
@@ -105,27 +118,34 @@ def _get_possible_paths(chunk_graph: Dict[str, List[str]], chunk_mapper: Dict[st
 
         # More than one children chunk
         heuristic_scores = {}
-        for child_chunk in children_chunks:
+        for idx, child_chunk in enumerate(children_chunks):
             # Checking if the child node is the end node
             if child_chunk == END_TOKEN: 
-                FLAG_END_TOKEN_CHILD = True
+                END_TOKEN_CHILD = idx
                 continue
             try:
                 heuristic_val = _get_heuristic_value(child_chunk, reverse_chunk_mapper, tf_idf_values, possible_paths[path_id])
-            except: heuristic_scores[child_chunk] = float('inf')
+            except UndefinedChunkError: 
+                # The exception is thrown for chunks which had been omitted during pre-processing.
+                # Judging from the code (keeping in mind that stop word removal has been set to false),
+                # the removed chunk can either be puctuation mark (like , ; : etc) or
+                # an abbreviation inside braces.
+                heuristic_val = float('inf')
             heuristic_scores[child_chunk] = heuristic_val
 
         reverse_sorted_chunks = sorted(heuristic_scores, key=heuristic_scores.get, reverse=True)
 
         for idx, chunk in enumerate(reverse_sorted_chunks):
             if idx < k:
-                if idx == 0 and not FLAG_END_TOKEN_CHILD: beam.append((chunk, path_id))
+                if idx == END_TOKEN_CHILD: continue
+                if idx == 0 or (END_TOKEN_CHILD == 0 and idx == 1): beam.append((chunk, path_id))
                 else: 
                     new_path_id = path_id + str(idx)
                     # Copying the previous path for the new diverged path
-                    possible_paths[new_path_id] = possible_paths[path_id]
+                    possible_paths[new_path_id] = copy(possible_paths[path_id])
                     beam.append((chunk, new_path_id))
 
+    logging.info('method: _get_possible_paths- Beam Search over, extracted paths in the graph')
     return list(possible_paths.values())
 
 def _reverse_chunk_map(chunk_map: Dict[str, List[str]]) -> Dict[str, str]:
@@ -140,17 +160,18 @@ def _get_heuristic_value(chunk: str, mapper: Dict[str, str], tf_idf_values: Dict
     processed_chunk = mapper.get(chunk)
     if processed_chunk is None: raise UndefinedChunkError('Chunk omitted during pre-processing.')
 
-    vectorized_chunk = vectorize_sentence(processed_chunk)
+    vectorized_chunk = vectorize_sentence(processed_chunk)[0]
     avg_tf_idf = sum(tf_idf_values.values()) / len(tf_idf_values)
 
-    for word in processed_chunk:
+    tf_idf_score = 0
+    for word in word_tokenize(processed_chunk):
         if tf_idf_values.get(word) is None:
             tf_idf_score += avg_tf_idf
         else: tf_idf_score += tf_idf_values[word]
     
     distances = []
     for prev_chunk in path:
-        vectorized_prev_chunk = vectorize_sentence(prev_chunk)
+        vectorized_prev_chunk = vectorize_sentence(prev_chunk)[0]
         distance = find_distance(vectorized_chunk, vectorized_prev_chunk)
         distances.append(distance)
 
